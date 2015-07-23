6ec4fb3a92fb2c71bb4a4e4d2cca2bde63ef56b6
Rohit Jha <rohit305jha@gmail.com>
2015-07-23 12:46:14 +0530

    Added Haddock documentation for Logic module.

--- src/Logic.hs
+++ src/Logic.hs
@@ -1,23 +1,22 @@
-{-
-------------------------
-| Logic Module for MPL |
-------------------------
+{-|
+Module      : Logic
+Description : Mathematical logic module for the MPL DSL
+Copyright   : (c) Rohit Jha, 2015
+License     : BSD2
+Maintainer  : rohit305jha@gmail.com
+Stability   : Stable
 
 Functionality for:
-  -> AND
-  -> OR
-  -> NOT
-  -> XOR
-  -> NAND
-  -> NOR
-  -> Logical Implication
-  -> Logical Equality
-  -> Operators for the above
-  -> Operations on list for the above
-
-Author: Rohit Jha
-Version: 0.1
-Date: 7 Feb 2013
+  * AND
+  * OR
+  * NOT
+  * XOR
+  * NAND
+  * NOR
+  * Logical Implication
+  * Logical Equality
+  * Operators for the above
+  * Operations on list for the above
 -}
 
 module Logic
@@ -45,58 +44,52 @@ module Logic
 where
 
 
--- Binary AND Function
-{-
-    Usage:
-        let a = True
-        let b = False
+{-|
+    The 'and'' function is for the binary AND operation.
 
-        c = and' a b
-        >>> c = False
+    For example:
+    
+    >>> let a = True
+    >>> let b = False
+    >>> c = and' a b
+    c = False
 
-        d = a `and'` b
-        >>> d = False
+    >>> True `and'` False
+    False
 -}
 and' :: Bool -> Bool -> Bool
 and' a b = a && b
 
 
--- Binary OR Function
-{-
-    Usage:
-        let a = True
-        let b = False
-        
-        let c = or a b
-        >>> c = True
-        
-        let d = a `or'` b
-        >>> d = True
+{-|
+    The 'or'' function is for the binary OR operation.
+    
+    For example:
+    
+    >>> let a = True
+    >>> let b = False
+    >>> let c = or a b
+    c = True
+    
+    >>> True `or'` False
+    True
 -}
 or' :: Bool -> Bool -> Bool
 or' a b = a || b
 
 
--- Unary NOT Function
--- Use 'not' from Prelude
-{-
-    Usage:
-        let b = not True
-        >>> b = False
--}
+{-|
+    The 'xor' function is for the binary XOR operation.
 
-
--- Binary XOR Function
-{-
-    Usage:
-        let a = True
-        let b = False
+    For example:
         
-        let c = xor a b
-        >>> c = True
-        
-        let d = a `xor` b
-        >>> d = True
+    >>> let a = True
+    >>> let b = False
+    >>> let c = xor a b
+    c = True
+    
+    >>> True `xor` False
+    True
 -}
 xor :: Bool -> Bool -> Bool
 xor a b
@@ -104,84 +97,90 @@ xor a b
   | otherwise = True
 
 
--- Binary XNOR Funtion
-{-
-    Usage:
-        xnor True False
-        >>> False
+{-|
+    The 'xnor' function is for the binary XNOR operation.
+
+    For example:
         
-        xnor True True
-        >>> True
-        
-        xnor False True
-        >>> False
-        
-        xnor False False
-        >>> True
+    >>> xnor True False
+    False
+    
+    >>> xnor True True
+    True
+    
+    >>> xnor False True
+    False
+    
+    >>> False `xnor` False
+    True
 -}
 xnor :: Bool -> Bool -> Bool
 xnor a b = not (xor a b)
 
 
--- Binary NAND Function
-{-
-    Usage:
-        let a = True
-        let b = False
-        
-        let c = nand a b
-        >>> c = True
-        
-        let d = a `nand` b
-        >>> d = True
+{-|
+    The 'nand' function is for the binary NAND operation.
+
+    For example:
+
+    >>> let a = True
+    >>> let b = False    
+    >>> let c = nand a b
+    c = True
+    
+    >>> True `nand` False
+    True
 -}
 nand :: Bool -> Bool -> Bool
 nand a b = not (a && b)
 
 
--- Binary NOR Function
-{-
-    Usage:
-        let a = True
-        let b = False
-        
-        let c = nor a b
-        >>> c = False
-        
-        let d = a `nor` b
-        >>> d = False
+{-|
+    The 'nor' function is for the binary NOR operation.
+
+    For example:
+
+    >>> let a = True
+    >>> let b = False
+    >>> let c = nor a b
+    c = False
+    
+    >>> True `nor` False
+    False
 -}
 nor :: Bool -> Bool -> Bool
 nor a b = not (a || b)
 
 
--- Binary Logical Equality
-{-
-    Usage:
-        let a = True
-        let b = False
-        
-        let c = equals a b
-        >>> c = False
-        
-        let d = a `equals` b
-        >>> d = False
+{-|
+    The 'equals' function checks for binary logical equality.
+
+    For example:
+
+    >>> let a = True
+    >>> let b = False
+    >>> let c = equals a b
+    c = False
+    
+    >>> True `equals` True
+    True
 -}
 equals :: Bool -> Bool -> Bool
 equals a b = a == b
 
 
--- Binary Logical Implication
-{-
-    Usage:
-        let a = True
-        let b = False
-        
-        let c = implies a b
-        >>> c = False
-        
-        let d = a `implies` b
-        >>> d = False
+{-|
+    The 'implies' function checks for binart logical implication.
+
+    For example:
+
+    >>> let a = True
+    >>> let b = False
+    >>> let c = implies a b
+    c = False
+    
+    >>> True `implies` False
+    False
 -}
 implies :: Bool -> Bool -> Bool
 implies a b
@@ -189,180 +188,203 @@ implies a b
   | otherwise = True
 
 
--- Binary and Operator
-{-
-    Usage:
-        True /\ True
-        >>> True
+{-|
+    The '/\' operator is the binary AND operator.
+
+    For example:
         
-        True /\ False
-        >>> False
-        
-        False /\ True
-        >>> False
-        
-        False /\ False
-        >>> False
+    >>> True /\ True
+    True
+    
+    >>> True /\ False
+    False
+    
+    >>> False /\ True
+    False
+    
+    >>> False /\ False
+    False
 -}
 (/\) :: Bool -> Bool -> Bool
 a /\ b = a && b
 
 
--- Binary or Operator
-{-
-    Usage:
-        True \/ True
-        >>> True
+{-|
+    The '\/' operator is the binary OR operator.
+
+    For example:
         
-        True \/ False
-        >>> True
-        
-        False \/ True
-        >>> True
-        
-        False \/ False
-        >>> False
+    >>> True \/ True
+    True
+    
+    >>> True \/ False
+    True
+    
+    >>> False \/ True
+    True
+    
+    >>> False \/ False
+    False
 -}
 (\/) :: Bool -> Bool -> Bool
 a \/ b = a || b
 
 
--- Binary implication Operator
-{-
-    Usage:
-        True ==> False
-        >>> False
-        
-        False ==> False
-        >>> True
-        
-        False ==> True
-        >>> True
-        
-        True ==> True
-        >>> True
+{-|
+    The '==>' operator is the binary implication operator.
+    This operator can be used instead of the 'implies' function.
+
+    For example:
+    
+    >>> True ==> False
+    False
+    
+    >>> False ==> False
+    True
+    
+    >>> False ==> True
+    True
+    
+    >>> True ==> True
+    True
 -}
 (==>) :: Bool -> Bool -> Bool
 a ==> b = implies a b
 
 
--- Binary equality Operator
-{-
-    Usage:
-        True <=> True
-        >>> True
+{-|
+    The '<=>' operator is the binary equality operator.
+
+    For example:
         
-        True <=> False
-        >>> False
-        
-        False <=> True
-        >>> False
-        
-        False <=> False
-        >>> True
+    >>> True <=> True
+    True
+    
+    >>> True <=> False
+    False
+    
+    >>> False <=> True
+    False
+    
+    >>> False <=> False
+    True
 -}
 (<=>) :: Bool -> Bool -> Bool
 a <=> b = a == b
 
 
--- unary not Operator on a list of Bool
-{-
-    Usage:
-        notL [True, True, False]
-        >>> [False,False,True]
-        
-        notL [True, False, True, True, False]
-        >>> [False,True,False,False,True]
-        
-        notL [True]
-        >>> [False]
-        
-        notL []
-        >>> []
+{-|
+    The 'notL' function is a unary NOT function that is applied to each element of a Bool list.
+
+    For example:
+
+    >>> notL [True, True, False]
+    [False,False,True]
+    
+    >>> notL [True, False, True, True, False]
+    [False,True,False,False,True]
+    
+    >>> notL [True]
+    [False]
+    
+    >>> notL []
+    []
 -}
 notL :: [Bool] -> [Bool]
 notL = map not
 
 
--- Binary and Operator on a list of Bool
-{-
-    Usage:
-        andL [True, False, True, True, False]
-        >>> False
-        
-        andL [True, True]
-        >>> True
-        
-        andL [False]
-        >>> False
+{-|
+    The 'andL' function is an AND operator that is applied on all elements of a Bool list.
+
+    For example:
+    
+    >>> andL [True, False, True, True, False]
+    False
+    
+    >>> andL [True, True]
+    True
+    
+    >>> andL [False]
+    False
 -}
 andL :: [Bool] -> Bool
 andL = and
 
 
--- Binary or Operator on a list of Bool
-{-
-    Usage:
-        orL [True, False, True, True, False]
-        >>> True
-        
-        orL [True, False]
-        >>> True
-        
-        orL [False, False]
-        >>> False
+{-|
+    The 'orL' function is an OR operator that is applied on all elements of a Bool list.
+
+    For example:
+    
+    >>> orL [True, False, True, True, False]
+    True
+    
+    >>> orL [True, False]
+    True
+    
+    >>> orL [False, False]
+    False
 -}
 orL :: [Bool] -> Bool
 orL = or
 
 
--- Binary xor Operator on a list of Bool
-{-
-    Usage:
-        xorL [True, False, True, True, False]
-        >>> True
-        
-        xorL [False, False]
-        >>> False
+{-|
+    The 'xorL' function is an XOR operator that is applied on all the elements of a Bool list.
+
+    For example:
+    
+    >>> xorL [True, False, True, True, False]
+    True
+    
+    >>> xorL [False, False]
+    False
 -}
 xorL :: [Bool] -> Bool
 xorL = foldl1 xor
 
 
--- Binary nand Operator on a list of Bool
-{-
-    Usage:
-        nandL [True, False, True, True, False]
-        >>> True
-        
-        nandL [False, False]
-        >>> True
+{-|
+    The 'nandL' function is a NAND operator that is applied on all the elements of a Bool list.
+
+    For example:
+    
+    >>> nandL [True, False, True, True, False]
+    True
+    
+    >>> nandL [False, False]
+    True
 -}
 nandL :: [Bool] -> Bool
 nandL = foldl1 nand
 
 
--- Binary nor Operator on a list of Bool
-{-
-    Usage:
-        norL [True, False, True, True, False]
-        >>> True
-        
-        norL [False, False]
-        >>> True
+{-|
+    The 'norL' function is a NOR operator that is applied on all the elements of a Bool list.
+
+    For example:
+
+    >>> norL [True, False, True, True, False]
+    True
+    
+    >>> norL [False, False]
+    True
 -}
 norL :: [Bool] -> Bool
 norL = foldl1 nor
 
 
--- Binary xnor Operator on a list of Bool
-{-
-    Usage:
-        xnorL [True, False, True, True, False]
-        >>> True
-        
-        xnorL [False, False]
-        >>> True
+{-|
+    The 'xnorL' function is an XNOR operator that is applied on all the elements of a Bool list.
+
+    For example:
+
+    >>> xnorL [True, False, True, True, False]
+    True
+    
+    >>> xnorL [False, False]
+    True
 -}
 xnorL :: [Bool] -> Bool
 xnorL = foldl1 xnor

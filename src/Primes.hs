908f40331dc880293135a3f3be792a1223d549c0
Rohit Jha <rohit305jha@gmail.com>
2015-08-01 00:19:39 +0530

    Added "areCoprime" function in Primes. Also added Haddock documentation for all the functions.

--- src/Primes.hs
+++ src/Primes.hs
@@ -1,18 +1,17 @@
-{-
--------------------------------
-| Prime Number Module for MPL |
--------------------------------
+{-|
+Module      : Primes
+Description : Prime Numbers module for the MPL DSL
+Copyright   : (c) Rohit Jha, 2015
+License     : BSD2
+Maintainer  : rohit305jha@gmail.com
+Stability   : Stable
 
 Functionality for:
-    -> Generating prime numbers upto a limit (Sieve of Eratosthenes)
-    -> Generate first 'n' primes
-    -> Generate the immediately next prime number greater than or equal to given number
-    -> Primality testing using Trial division and Miller-Rabin test
-    -> Prime factorization
-
-Author: Rohit Jha
-Version: 0.2
-Date: 17 Jun 2015
+    * Generating prime numbers upto a limit (Sieve of Eratosthenes)
+    * Generate first 'n' primes
+    * Generate the immediately next prime number greater than or equal to given number
+    * Primality testing using Trial division and Miller-Rabin test
+    * Prime factorization
 -}
 
 module Primes
@@ -21,24 +20,26 @@ module Primes
     primesBetween,
     nPrimes,
     primesTo100,
-    trialDivision,
     primesTo10000,
     isTrialDivisionPrime,
-    isStrongPseudoPrime,
     isMillerRabinPrime,
     isPrime,
     nextPrime,
     primeFactors,
-    uniquePrimeFactors
+    uniquePrimeFactors,
+    areCoprime
 )
 where
 
 import Data.List (nub)
 
+
 -- internal functions ----------------------
+divides :: Integral a => a -> a -> Bool
 d `divides` n = n `mod` d == 0
 
 
+splitWith :: (Integral t, Num t1) => t -> t -> (t1, t)
 n `splitWith` p = doSplitWith 0 n
     where
         doSplitWith s t
@@ -46,6 +47,7 @@ n `splitWith` p = doSplitWith 0 n
             | otherwise     = (s, t)
 
 
+power :: Integral a => (t, t -> t -> t) -> t -> a -> t
 power (idG, multG) = doPower idG
     where
         doPower y _ 0 = y
@@ -58,6 +60,7 @@ power (idG, multG) = doPower idG
             in doPower y' x' n'
 
 
+minus :: Ord a => [a] -> [a] -> [a]
 minus (x:xs) (y:ys) = case compare x y of 
            LT -> x : minus  xs  (y:ys)
            EQ ->     minus  xs     ys 
@@ -81,19 +84,23 @@ primePowerFactors n | n > 0 = takeOutFactors n primesTo10000
             else error ("primePowerFactors: unable to factor " ++ show n)
 
 
+sieve :: (Enum a, Num a, Ord a) => [a] -> [a]
 sieve (p:xs) 
        | p*p > last xs   = p : xs
        | otherwise = p : sieve (xs `minus` [p*p, p*p+2*p..])
 ----------------------------------------------
 
 
--- Generate list of primes upto specified limit by using Sieve of Eratosthenes
-{-
-    Usage:
-        primesTo 20
-        => [2,3,5,7,11,13,17,19]
-        primesTo 76
-        => [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73]
+{-|
+    The 'primesTo' function generates a list of prime numbers upto a specified limit by using Sieve of Eratosthenes
+
+    For example:
+
+    >>> primesTo 20
+    [2,3,5,7,11,13,17,19]
+    
+    >>> primesTo 76
+    [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73]
 -}
 primesTo :: Integer -> [Integer]
 primesTo m
@@ -101,20 +108,24 @@ primesTo m
     | m < 2 = []
     | m == 2 = [2]
     | m < 5 = [2,3]
+    | m == 100 = primesTo100
+    | m == 10000 = primesTo10000
     | otherwise = 2 : sieve [3,5..m]
 
 
--- Generate all primes between two numbers (upper limit inclusive)
-{-
-    Usage:
-        primesBetween 0 10
-        >>> [2,3,5,7]
+{-|
+    The 'primesBetween' function returns all prime numbers between the mention lower and upper limits (upper limit inclusive)
+    
+    For example:
 
-        primesBetween 10 25
-        >>> [11,13,17,19,23]
+    >>> primesBetween 0 10
+    [2,3,5,7]
 
-        primesBetween 5 2
-        >>> [2,3,5]
+    >>> primesBetween 10 25
+    [11,13,17,19,23]
+
+    >>> primesBetween 5 2
+    [2,3,5]
 -}
 primesBetween :: Integer -> Integer -> [Integer]
 primesBetween m n
@@ -124,47 +135,65 @@ primesBetween m n
     | otherwise = primesTo n `minus` primesTo m --(nextPrime m) : sieve [((nextPrime m)+1) .. n]
 
 
--- Generate list of first 'n' primes
-{-
-    Usage:
-        nPrimes 10
-        => [2,3,5,7,11,13,17,19,23,29]
-        nPrimes 42
-        => [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181]
+{-|
+    The 'nPrimes' function returns a list of first 'n' primes, where 'n' is the argument passed.
+
+    For example:
+
+    >>> nPrimes 10
+    [2,3,5,7,11,13,17,19,23,29]
+
+    >>> nPrimes 42
+    [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181]
 -}
 nPrimes n = take n (sieve [2..])
     where sieve (p:ns) = p : sieve (filter (notdiv p) ns)
           notdiv p n = n `mod` p /= 0
 
 
--- List of prime numbers under 100
+{-|
+    The 'primesTo100' function returns a list of all prime numbers less than 100.
+-}
 primesTo100 :: [Integer]
 primesTo100 = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
 
 
--- Trial division
+-- internal function for trial division
+trialDivision :: Integral a => [a] -> a -> Bool
 trialDivision ps n = doTrialDivision ps
     where doTrialDivision (p:ps) = let (q,r) = n `quotRem` p in ((r /= 0) && ((q < p) || doTrialDivision ps))
           doTrialDivision [] = True
 
 
--- List of prime numbers under 10000
+{-|
+    The 'primesTo100' function returns a list of all prime numbers less than 10000.
+-}
+primesTo10000 :: [Integer]
 primesTo10000 = primesTo100 ++ filter (trialDivision primesTo100) [101,103..9999]
 
 
--- Determine primality using trial division
-{-
-    Usage:
-        isTrialDivisionPrime 83498594
-        => False
-        isTrialDivisionPrime 83498594823549879237544
-        => False
+{-|
+    The 'isTrialDivisionPrime' function is checks the primality of a number using trial division.
+    
+    For example:
+
+    >>> isTrialDivisionPrime 83498594
+    False
+
+    >>> isTrialDivisionPrime 83498617
+    True
+
+    >>> isTrialDivisionPrime 83498594823549879237544
+    False
 -}
+isTrialDivisionPrime :: Integer -> Bool
 isTrialDivisionPrime 2 = True
 isTrialDivisionPrime n = trialDivision (primesTo10000 ++ [10001,10003..]) n
 
 
--- Check if number is a pseudo-prime (probable)
+{-|
+    The 'isStrongPseudoPrime' function is an internal function to check if number is a pseudo-prime (probable)
+-}
 isStrongPseudoPrime :: Integer -> (Int,Integer) -> Integer -> Bool
 isStrongPseudoPrime n (s,t) b =
     let b' = power (1, \x y -> x*y `mod` n) b t
@@ -177,13 +206,16 @@ isStrongPseudoPrime n (s,t) b =
             | otherwise = doSquaring (s-1) (x*x `mod` n)
 
 
--- Check if number is prime using Miller-Rabin primality test
-{-
-    Usage:
-        isMillerRabinPrime 34658273497773875
-        => False
-        isMillerRabinPrime 34658273497773875327483287487877599
-        => True
+{-|
+    The 'isMillerRabinPrime' function checks if number is prime using Miller-Rabin primality test
+    
+    For example:
+
+    >>> isMillerRabinPrime 34658273497773875
+    False
+    
+    >>> isMillerRabinPrime 34658273497773875327483287487877599
+    True
 -}
 isMillerRabinPrime :: Integer -> Bool
 isMillerRabinPrime n
@@ -192,15 +224,21 @@ isMillerRabinPrime n
         where (s,t) = (n-1) `splitWith` 2
 
 
--- Primality Checking which uses appropriate test according to the given number
-{-
-    Usage:
-        isPrime (5^100)
-        => False
-        isPrime 3973793488238
-        => False
-        isPrime 199
-        => True
+{-|
+    The 'isPrime' function checks if a number is prime.
+
+    The function uses the 'isTrialDivisionPrime' or 'isMillerRabinPrime' functions to test the primality.
+
+    For example:
+
+    >>> isPrime (5^100)
+    False
+    
+    >>> isPrime 3973793488238
+    False
+    
+    isPrime 199
+    >>> True
 -}
 isPrime :: Integer -> Bool
 isPrime n
@@ -209,13 +247,16 @@ isPrime n
     | n >= 500000000 = isMillerRabinPrime n
 
 
--- Generate the next prime greater than or equal to the given number
-{-
-    Usage:
-        nextPrime 10240
-        => 10243
-        nextPrime 3894345
-        => 3894347
+{-|
+    The 'nextPrime' function returns the next prime greater than or equal to the passed argument.
+    
+    For example:
+
+    >>> nextPrime 10240
+    10243
+    
+    >>> nextPrime 3894345
+    3894347
 -}
 nextPrime :: Integer -> Integer
 nextPrime n
@@ -223,13 +264,18 @@ nextPrime n
     | n < 2 = 2
 
 
--- Prime factorization of a number
-{-
-    Usage:
-        primeFactors 23984
-        => [2,2,2,2,1499]
-        primeFactors 273219943
-        => [19,89,161573]
+{-|
+    The 'primeFactors' function returns all the prime factors of a number.
+
+    The function includes repetitions of factors, as shown in the examples below. To get only the unique factors, use the 'uniquePrimeFactors' function.
+
+    For example:
+    
+    >>> primeFactors 23984
+    [2,2,2,2,1499]
+    
+    >>> primeFactors 273219943
+    [19,89,161573]
 -}
 primeFactors :: Integer -> [Integer]
 primeFactors n
@@ -238,15 +284,32 @@ primeFactors n
     | otherwise = concatMap (\ (p, a) -> replicate a p) (primePowerFactors n)
 
 
--- Unique prime factors of a number
-{-
-    Usage:
-        uniquePrimeFactors 10000000
-        >>> [2,5]
-        uniquePrimeFactors 123456789
-        >>> [3,3607,3803]
+{-|
+    The 'uniquePrimeFactors' function returns a single instance of all the prime factors of a number.
+
+    For example:
+    
+    >>> uniquePrimeFactors 10000000
+    [2,5]
+    >>> uniquePrimeFactors 123456789
+    [3,3607,3803]
 -}
 uniquePrimeFactors :: Integer -> [Integer]
 uniquePrimeFactors n
     | n < 1 = error "Usage - uniquePrimeFactors n, where n is a positive number."
     | otherwise = nub $ primeFactors n 
+
+
+{-|
+    The 'areCoprime' function checks if two numbers are coprime / relatively prime / mutually prime.
+
+    For example:
+
+    >>> areCoprime 2 3
+    True
+
+    >>> areCoprime 100 200
+    False
+-}
+areCoprime :: Integer -> Integer -> Bool
+areCoprime a b = gcd a b == 1
